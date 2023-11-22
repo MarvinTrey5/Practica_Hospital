89f971157e100beab484d79bb50470b8d8cf914a
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-21 20:20:40 -0600

    Carpeta formularios, se comentaron las clases

--- app_hospital/formularios/add_medic.py
+++ app_hospital/formularios/add_medic.py
@@ -1,5 +1,5 @@
 from django import forms
 class Add_medic(forms.Form):
-    nombre=forms.CharField(max_length=100)
-    apellido=forms.CharField(max_length=100)
-    especialidad=forms.CharField(max_length=100)
+    nombre=forms.CharField(max_length=100) # LLave para el campo del médico tipo varchar longitud 100 carácteres.
+    apellido=forms.CharField(max_length=100) # LLave para el campo de apellido del médico tipo varchar longitud 100 carácteres.
+    especialidad=forms.CharField(max_length=100) # Llave para el campo de especialidad del médico tipo varchar longitud 100 carácteres.
--- app_hospital/formularios/add_pacient.py
+++ app_hospital/formularios/add_pacient.py
@@ -1,8 +1,8 @@
 from django import forms
 class Add_pacient(forms.Form):
-    nombre=forms.CharField(max_length=100)
-    apellido=forms.CharField(max_length=100)
-    fecha_nacimiento=forms.DateTimeField()
-    sexo=forms.CharField(max_length=20)
-    altura=forms.FloatField()
-    medico_id = forms.IntegerField()
+    nombre=forms.CharField(max_length=100) # Llave para el nombre del campo del paciente tipo varchar longitud 100 carácteres.
+    apellido=forms.CharField(max_length=100) # LLave para el apellido del campo del paciente tipo varchar longitud 100 carácteres.
+    fecha_nacimiento=forms.DateTimeField() # Llave para el campo de fecha_nacimiento del paciente tipo DateTimeField consta de ej: 2002-05-29 12:00:00
+    sexo=forms.CharField(max_length=20) # Llave para el campo de sexo del paciente tipo varchar longitud 20 carácteres.
+    altura=forms.FloatField() # Llave para el campo de altura del paciente tipo float ej: 1.84.
+    medico_id = forms.IntegerField() # Llave para el camppo de medico_id que se relaciona con el campo id de la tabla medico tipo entero.
