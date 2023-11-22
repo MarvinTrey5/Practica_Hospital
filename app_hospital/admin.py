fdd2ffa007353a0b2e6590a21df4397d87a2cf50
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-22 07:17:33 -0600

    Vistas para añadir datos desde pgAdmin

--- app_hospital/admin.py
+++ app_hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import medico, paciente
 # Register your models here.
-admin.site.register(medico)
-admin.site.register(paciente)
+admin.site.register(medico) #Para añadir en pgAdmin desde el login para médico.
+admin.site.register(paciente) #Para añadir en pgAdmin desde el login para médico.
